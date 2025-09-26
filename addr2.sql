5b9ed74f785177a297a208db9cf1dd09c613756a
QuanghocIT2k4 <ndnquang3072004@gmail.com>
2025-09-26 15:33:52 +0700

    feat: update buyer cart/order + fix b2c

--- addr2.sql
+++
@@ -1,1 +0,0 @@
-USE e-commerce; INSERT INTO addresses (id, province, district, ward, home_address) VALUES ('38f47610-3c41-4abb-9222-fc098761118d','Ha Noi','Cau Giay','Dich Vong','So 2 Duong C');
--- get_store.sql
+++
@@ -1,1 +0,0 @@
-SELECT id FROM stores WHERE owner_id='dca87bd3-615a-4a0c-b4d5-2d91ac8f9786' ORDER BY created_at DESC LIMIT 1;
